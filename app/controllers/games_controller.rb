56ccd89a9aa092405df894aa50b45ecfd75d8265
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-06 14:41:35 +0100

    Generates and displays 10 randoms letters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,11 @@
 class GamesController < ApplicationController
   def new
+    @letters = []
+    i = 0
+    while i < 10
+      @letters << ('A'..'Z').to_a.sample
+      i += 1
+    end
   end
 
   def score
